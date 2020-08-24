1d9db67429e8164d3954fe94bf81a20ba66dcae8
hytw06 <hytw06@163.com>
2020-08-24 09:46:16 +0800

    修改文件配置类

--- src/main/java/com/axe/trace/sys/config/FileUploadConfig.java
+++ src/main/java/com/axe/trace/sys/config/FileUploadConfig.java
@@ -6,7 +6,7 @@ package com.axe.trace.sys.config;
 public class FileUploadConfig {
 
     // 服务器文件访问IP、端口、根路径
-    public static final String SERVER_IP_PORT = "http://112.124.22.250:8081/trace/files";
+    public static final String SERVER_IP_PORT = "http://112.124.22.250:8081/trace/files/";
 
     // 文件保存的根路径
     public static final String UPLOAD_BASE_PATH = "/root/develop/files/";
